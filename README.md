090e09e0965823c2e3aec1ac77081fc282b89aa4
Matthias Gaertner <g@rtner.de>
2021-04-04 22:28:34 +0200

    Initial commit, converted 2014-2021

--- README.md
+++ README.md
@@ -1,2 +1,106 @@
 # DBKontoauszugReader
 Decode Deutsche Bank Kontoauszug PDF files into JSON format.
+
+# Why?
+
+I have local copies of my bank statements from Deutsche Bank, **in PDF format**, including pretty old ones.
+
+To do some stats and also to facilitate easier __searching__ through those statements, I whipped up a small program to convert the PDFs to JSON format.
+
+Having them in textual JSON representation allows to post-process them using JSON tooling, e.g. "jq".
+# Why not?
+
+* You can download CSV representations of these statements from the DB online banking. Try that first. Not sure if you can access __old__ statements this way, though.
+* Obviously, there are other solutions to make PDFs searchable. Apache Tika, Elasticsearch with plug-ins, Adobe Tools, or even your OS may give you search options already...
+
+# Building Blocks
+
+* Java JRE / JDK
+* Apache PDFBox https://pdfbox.apache.org/
+* A valuable hint on StackOverflow to make PDFBox decode these (broken) PDFs properly: https://stackoverflow.com/a/45922162/1037626
+* Builds using Gradle, but since it's only one file and a handfulf of JARs, you could probably build this by hand as well.
+* Note: I am using Java 11 currently. YMMV with other versions, I guess anything beyond Java 8 including should work.
+
+# Synopsis
+
+    java de.m9aertner.dbkr.DBKontoauszugReader <options> <one or more folders, PDF files>
+
+      -r             Recursive mode. Given a folder on the command line, recurse into subfolders.
+                     Default is to look into the given folder, but to not recurse deeper.
+      -p             Prefix. Consider only files whose name have this prefix, e.g. -p Kontoauszug_
+      -q             Quiet. Do not report the number of booking lines written at the end of the run.
+      -v             Verbose mode. Show which files have been (re-)created. Only with -d and -j modes.
+      -j             JSON mode. Put a *.json file next to any input *.pdf file, with all the bookings
+                     from that PDF in one JSON.
+      -1             JSON single-line mode. Default is to pretty-print the generated JSON.
+      -o             Output file mode. Send all JSONs to this output file. Default is stdout.
+      -d             Directory mode. Create a YYYY/MM/DD folder and put each booking line into its own
+                     numbered JSON file there.
+      -m             Month only. Do not create DD (day) folders in directory mode.
+      -b             For directory mode, the JSON file name is derived from the booking line valuta
+                     date (YYYY-MM-DD-nnnnnnnn.json)
+                     With this setting active, the booking date is used instead.
+      -u             Update/overwrite target file. Default is to not touch pre-existing target files.
+      --version, -V  Show DBKontoauszugReader version, then quit.
+      --             Mark end of options processing, all remaining command line items will be treated
+                     as input.
+      
+
+# Building / Running
+
+Build using gradlew. Or use Eclipse. Or use IDEA. Or just "javac".
+
+I've just set up a small shell script / batch file that sets up the JARs a, then does the conversions for __my__ (private) folder structures as required.
+
+If you're more into Gradle, use the "distZip" mechanism to create a "distribution". For small ad-hoc stuff like this, I did not bother.
+
+    SET CLASSPATH=C:\...\DBKontoauszugReader\app\bin\main
+    SET CLASSPATH=%CLASSPATH%;C:\Users\...\.gradle\caches\modules-2\files-2.1\commons-logging\commons-logging\1.2\4bfc12adfe4842bf07b657f0369c4cb522955686\commons-logging-1.2.jar
+    SET CLASSPATH=%CLASSPATH%;C:\Users\...\.gradle\caches\modules-2\files-2.1\org.apache.pdfbox\fontbox\2.0.23\1a6b960dd2c1b1f8a5f5d6668b2930b50ff4324d\fontbox-2.0.23.jar
+    SET CLASSPATH=%CLASSPATH%;C:\Users\...\.gradle\caches\modules-2\files-2.1\org.hamcrest\hamcrest-core\1.3\42a25dc3219429f0e5d060061f71acb49bf010a0\hamcrest-core-1.3.jar
+    SET CLASSPATH=%CLASSPATH%;C:\Users\...\.gradle\caches\modules-2\files-2.1\com.fasterxml.jackson.core\jackson-annotations\2.12.2\a770cc4c0a1fb0bfd8a150a6a0004e42bc99fca\jackson-annotations-2.12.2.jar
+    SET CLASSPATH=%CLASSPATH%;C:\Users\...\.gradle\caches\modules-2\files-2.1\com.fasterxml.jackson.core\jackson-core\2.12.2\8df50138521d05561a308ec2799cc8dda20c06df\jackson-core-2.12.2.jar
+    SET CLASSPATH=%CLASSPATH%;C:\Users\...\.gradle\caches\modules-2\files-2.1\com.fasterxml.jackson.core\jackson-databind\2.12.2\5f9d79e09ebf5d54a46e9f4543924cf7ae7654e0\jackson-databind-2.12.2.jar
+    SET CLASSPATH=%CLASSPATH%;C:\Users\...\.gradle\caches\modules-2\files-2.1\junit\junit\4.13.1\cdd00374f1fee76b11e2a9d127405aa3f6be5b6a\junit-4.13.1.jar
+    SET CLASSPATH=%CLASSPATH%;C:\Users\...\.gradle\caches\modules-2\files-2.1\org.apache.pdfbox\pdfbox\2.0.23\b89643d162c4e30b4fe39cfa265546cc506d4d18\pdfbox-2.0.23.jar
+
+# Input
+
+The PDF format that I have here seems to have been pretty stable over the last decade at least.
+
+It's the Deutsche Bank PDFs with
+* Customer address,
+* Filiale address, (hard to decode as intermingled with the customer address on the same line in the intermediate text representation)
+* "Kontoauszug",
+* "Kontoinhaber", 
+* Auszug#, Seite#, IBAN, BIC, "Alter Saldo",
+* "Buchung Valuta Vorgang Soll Haben", (this is actually a key marker line which triggers the decoding)
+* Booking lines...
+* ... delimited at the bottom of each page by the "vertical magic numbers" on the bottom left of the page,
+* or by the trailing "Filialnummer / Kontonummer" line.
+
+# Sample Output
+
+    Command:
+    java de.m9aertner.dbkr.DBKontoauszugReader -v -p Kontoauszug -m -d C:\Bank\DeutscheBankJson -- C:\Bank\DeutscheBank
+
+    Output in C:\Bank\DeutscheBankJson\2014\03\2014-03-04-00000001.json, one file per booking line, e.g.:
+    {
+      "pdfName" : "Kontoauszug_950.........EUR_2014-03-31_KK_950.........KD401V050401010.........pdf",
+      "pdfLine" : "12",
+      "buchung" : "04.03.2014",
+      "valuta" : "04.03.2014",
+      "text" : "SEPA Dauerauftrag an ............... IBAN DE..................67 BIC ........XXX Verwendungszweck/ Kundenreferenz ................ RINP Dauerauftrag",
+      "amount" : "- 75,00",
+      "amountCt" : -7500
+    }
+
+# Caveats
+
+* The text extraction logic expects German language "Kontoauszug" PDFs only. If you live elsewhere and have an account with Deutsche Bank, the statements may be localized, which _will_ break the super-simple decoder here.
+* The booking line texts are collapsed onto a single line, with a single space character inserted between the fragments ('lines'). Unfortunately, the Deutsche Bank renderer introduces hard word breaks with no discernible identification. Hence, the inserted space can break 'words' into pieces, inadvertently. In other words, expect some spurious spaces in the "text" section.
+* Not all data is extracted from the PDF: since I apply this only to my single bank account, no IBAN detection is in place and no names are decoded. Also the saldi are not decoded.
+* I wonder why Deutsche Bank does not embed machine-readable original content right into the PDFs, as embedded PDF content (XML, BER, EDI, whatever.). Such content might actually be  _present_, but I have not looked for it (yet). Properly dissecting these PDF files on even lower level, anyone?
+* Deutsche Bank offers a "Demokonto" where you can get an impression about how their online banking _looks and feels_. I think this is actually a pretty nice idea. All static, though.Unfortunately, the button to download or view a bank statement PDF is not operational. Instead of a PDF document, a dialog box pops up indicating that this function is not available for the demonstration account. Why not just deliver a sample PDF with mock data, albeit formatted properly?
+
+# Enjoy!
---
+++ app/src/main/java/de/m9aertner/dbkr/DBKontoauszugReader.java
@@ -0,0 +1,625 @@
+/**
+ * MIT License
+ *
+ * Copyright 2021 Matthias Gärtner
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+package de.m9aertner.dbkr;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.StringReader;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.cos.COSDictionary;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.cos.COSObject;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDResources;
+import org.apache.pdfbox.pdmodel.graphics.PDXObject;
+import org.apache.pdfbox.pdmodel.graphics.form.PDFormXObject;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+/**
+ * Simply application of Apache PDFBox PDF decoder framework to decode Deutsche
+ * Bank Kontoauszüge (bank statements) into machine-readable JSON documents.
+ *
+ * This is a command line application.
+ *
+ * One or more <code>*.PDF</code> files can be specificed on the command line.
+ *
+ * Recursion into folders is supported (-r). File names found during recursion
+ * can optionally be checked against a prefix (e.g. "Kontoauszug_") so that
+ * other PDFs in the source folder(s) are ignored.
+ *
+ * Output can be produced three ways: Normal operation decodes all bank
+ * statements and their bank statement lines into an empty-line delimited stream
+ * of JSON elements. Note that this is a true JSON file only for single input
+ * PDFs. With multiple PDFs as input, it is a concatenation of JSON files,
+ * suitable for post-processing, e.g. using "jq". Default output goes to stdout,
+ * but an output file can be specified using the -o option.
+ *
+ * The second mode, a folder is created for each year/month[/day] for which a
+ * booking line has been detected. This is enabled using -d with a base
+ * directory. Day granularity is default, month granularity can be selected
+ * using -m option. The "valuta" date is used as default for the relevant date,
+ * using -b the "booking date" can be chosen as well. Target files that do exist
+ * already will not be overwritten, unless -u (update) option is given.
+ *
+ * A third mode is to produce a *.json file next to the original PDF file (-j).
+ * The PDF file name is kept, extension is changed, and all booking lines from
+ * that PDF will end up in one JSON file.
+ *
+ * You can indicate end of options with double dash (--). File name arguments
+ * starting with an octothorpe (#) and options starting with '-#' will be ignored
+ * silently.
+ */
+public class DBKontoauszugReader {
+
+	public static final String VERSION = "1.0.0";
+
+	// default output stream, ignored with -d
+	protected PrintStream out = null;
+
+	// -q: Prevent number of booking lines processed from being output on out.
+	protected boolean quiet = false;
+
+	// -v: Verbose, emit generated file names, only for directory mode (-d)
+	protected boolean verbose = false;
+
+	// -p: default accept-all file filter, replaces with -p (prefix) option.
+	protected FileFilter fileFilter = pathname -> true;
+
+	// Maintain number of booking lines processed. One "Kontoauszug" will contain
+	// zero, one or more such lines.
+	protected int n = 0;
+
+	// When using -o, we should close the output stream again.
+	protected boolean closeStream = false;
+
+	// -u: default do not overwrite, use with -d and -j
+	protected boolean update = false;
+
+	// -j: create *.json file next to original PDF file, mutually exclusive with -d
+	// and -o
+	protected boolean jsonMode = false;
+
+	// -m: default day level = true, use with -d
+	protected boolean month = false;
+
+	// -b: default use valuta date for folder/file name generation, use with -d
+	protected boolean buchungDate = false;
+
+	// -1: default pretty printed JSON, -1 emity one-line JSON
+	protected boolean oneLine = false;
+
+	// -d: Folder-per-month or folder-per-day base directory, set via -d
+	protected File baseDir;
+
+	// default no recursion, applies to folder arguments
+	protected boolean recurse = false;
+
+	public static void main(String[] args) throws Exception {
+		DBKontoauszugReader c = new DBKontoauszugReader();
+		c.out = System.out;
+		c.handleOptionsAndRun(args);
+		if (!c.quiet) {
+			c.out.println("Number of booking lines processed: " + c.n);
+		}
+	}
+
+	public void handleOptionsAndRun(String[] args) throws Exception {
+		boolean options = true;
+		for (int i = 0; i < args.length; i++) {
+			String arg = args[i];
+
+			if (options && arg.equals("--")) {
+				options = false;
+			} else if (options && arg.startsWith("-")) {
+				if (!arg.startsWith("-#")) {
+					i = handleOption(arg, args, i);
+				}
+			} else {
+				if (jsonMode && closeStream) {
+					throw new IllegalArgumentException("Json mode is mutually exclusive with output file -o.");
+				}
+				if (jsonMode && baseDir != null) {
+					throw new IllegalArgumentException("Json mode is mutually exclusive with directory mode -d.");
+				}
+				if (update && baseDir == null && !jsonMode) {
+					throw new IllegalArgumentException(
+							"Update flag -u can only be used with output directory -d or -j.");
+				}
+				if (month && baseDir == null) {
+					throw new IllegalArgumentException("Month flag -m can only be used with output directory -d.");
+				}
+				options = false;
+				handleArgument(arg);
+			}
+		}
+		if (closeStream) {
+			out.close();
+		}
+	}
+
+	/**
+	 * Handle one option
+	 * 
+	 * @param arg  Current option, e.g. '-r'
+	 * @param args All options, to get the option's value, if any
+	 * @param i    current option index
+	 * @return updated option index, for options with value(s)
+	 * @throws Exception
+	 */
+	protected int handleOption(String arg, String[] args, int i) throws Exception {
+		if ("-r".equals(arg)) {
+			recurse = true;
+		} else if ("-p".equals(arg)) {
+			fileFilter = new PrefixFileFilter(args[++i]);
+		} else if ("-q".equals(arg)) {
+			quiet = true;
+		} else if ("-v".equals(arg)) {
+			verbose = true;
+		} else if ("-j".equals(arg)) {
+			jsonMode = true;
+		} else if ("-u".equals(arg)) {
+			update = true;
+		} else if ("-m".equals(arg)) {
+			month = true;
+		} else if ("-b".equals(arg)) {
+			buchungDate = true; // default to ValutaDate
+		} else if ("-1".equals(arg)) {
+			oneLine = true; // default to pretty-print
+		} else if ("-o".equals(arg)) {
+			if (baseDir != null) {
+				throw new IllegalArgumentException(
+						"Output file -o and output directory -d options are mutually exclusive.");
+			}
+			if (closeStream) {
+				out.close();
+			}
+			out = new PrintStream(new FileOutputStream(args[++i]));
+			closeStream = true;
+		} else if ("-d".equals(arg)) {
+			if (closeStream) {
+				throw new IllegalArgumentException(
+						"Output file -o and output directory -d options are mutually exclusive.");
+			}
+			baseDir = new File(args[++i]);
+			if (!baseDir.isDirectory()) {
+				throw new IllegalArgumentException("Not a directory: " + baseDir);
+			}
+		} else if ("--version".equals(arg) || "-V".equals(arg)) {
+			out.println("DBKontoauszugReader " + VERSION);
+			i = 99999;
+			quiet = true;
+		}
+		return i;
+	}
+
+	/**
+	 * Handles the next argument (command line file name)
+	 * 
+	 * @param arg
+	 * @throws Exception
+	 */
+	protected void handleArgument(String arg) throws Exception {
+		if (!arg.startsWith("#")) { // hash marks a a comment (inactive) argument
+			File f = new File(arg);
+			if (f.exists()) {
+				visit(f, true, false);
+			} else {
+				throw new FileNotFoundException(arg);
+			}
+		}
+	}
+
+	/**
+	 * Visit one file. Process if it's a file, recurse if it's a folder.
+	 * 
+	 * @param f              May be a folder or file.
+	 * @param recurse        if <code>true</code>, recurse into folder,
+	 *                       <code>false</code> ignore folders.
+	 * @param checkExtension <code>true</code> check PDF file extension,
+	 *                       <code>false</code> do not check. Note that files given
+	 *                       on the command line are not checked.
+	 * @throws Exception
+	 */
+	protected void visit(File f, boolean recurse, boolean checkExtension) throws Exception {
+		if (f.isFile()) {
+			if (!checkExtension || f.getName().toLowerCase().endsWith(".pdf")) {
+				visitFile(f);
+			}
+		} else if (recurse && f.isDirectory()) {
+			// Recurse
+			File[] listFiles = f.listFiles(fileFilter);
+			for (int j = 0; j < listFiles.length; j++) {
+				visit(listFiles[j], this.recurse, true);
+			}
+		}
+	}
+
+	/**
+	 * Extract the text from the given file, then emit the booking, if any.
+	 * 
+	 * @param file
+	 * @throws Exception
+	 */
+	protected void visitFile(File file) throws Exception {
+		String text = convertPDFtoText(file);
+		Booking b = decodeBooking(text, file);
+		if (b != null) {
+			emitBooking(b);
+		}
+	}
+
+	/**
+	 * Use PDFBox to extract all the text content from the file.
+	 * <p>
+	 * Note that we need to "fix" the broken ToUnicodeMap encoding of Deutsche Bank
+	 * PDF files so that the decoder falls back to the simple WinAnsiEncoding, which
+	 * works.
+	 *
+	 * @see https://stackoverflow.com/a/45922162/1037626
+	 * @param file
+	 * @return
+	 * @throws Exception
+	 */
+	protected String convertPDFtoText(File file) throws Exception {
+		String text = null;
+		try (PDDocument document = PDDocument.load(file)) {
+
+			for (int pageNr = 0; pageNr < document.getNumberOfPages(); pageNr++) {
+				PDPage page = document.getPage(pageNr);
+				PDResources resources = page.getResources();
+				removeToUnicodeMaps(resources);
+			}
+
+			PDFTextStripper stripper = new PDFTextStripper();
+			stripper.setSortByPosition(true);
+			text = stripper.getText(document);
+		}
+		return text;
+	}
+
+	public static final Pattern P0 = Pattern.compile("^Kontoauszug vom " //
+			+ "((\\d\\d)\\.(\\d\\d)\\.\\d{4}) bis " //
+			+ "((\\d\\d)\\.(\\d\\d)\\.\\d{4})");
+
+	public static final Pattern P1 = Pattern.compile("^00\\d{8,} / \\d{8,} / \\d{8,}");
+
+	public static final Pattern P2 = Pattern.compile("^Buchung Valuta Vorgang Soll Haben");
+
+	public static final Pattern P3 = Pattern.compile("^" //
+			+ "((\\d\\d)\\.(\\d\\d)\\.) " //
+			+ "((\\d\\d)\\.(\\d\\d)\\.) " //
+			+ "(.+) " //
+			+ "((-|\\+) \\d+(\\.\\d+)*,\\d\\d)");
+
+	public static final Pattern P4 = Pattern.compile("^Filialnummer Kontonummer Neuer Saldo");
+
+	/**
+	 * Primitive state machine that decodes key lines from the PDF text line stream.
+	 * Obviously, this is pretty <b>incomplete</b>, but it's <i>enough for my
+	 * needs</i>.
+	 * <p>
+	 * Feel free to enhance this, e.g. by reading totals, account number, contact
+	 * names et cetera.
+	 * <p>
+	 * I believe it is not possible to recover the <b>exact</b> contents of the
+	 * booking line's text by just looking at the text extraction. Especially "line
+	 * breaks" appear to take into account the non-monospace letter width of lines
+	 * and if that overflows, a hard word-splitting break gets inserted.
+	 * 
+	 * @param text
+	 * @param file
+	 * @return
+	 * @throws Exception
+	 */
+	protected Booking decodeBooking(String text, File file) throws Exception {
+		int Z = 0;
+		Booking b = null;
+		BookingLine bl = null;
+		BufferedReader br = new BufferedReader(new StringReader(text));
+		while (Z >= 0) {
+			String line = br.readLine();
+			if (line == null) {
+				break;
+			}
+			if (Z == 0) {
+				Matcher m = matchLine(P0, line); // Statement header line (from, to)
+				if (m != null) {
+					b = new Booking();
+					b.from = m.group(1);
+					b.to = m.group(4);
+					b.pdfFile = file;
+					Z = 1;
+					continue;
+				}
+			}
+			if (Z == 1 || Z == 2 || Z == 3) {
+				Matcher m = matchLine(P1, line); // End of page marker, i.e. vertical small print on bottom left
+				if (m != null) {
+					if (bl != null) {
+						b.lines.add(bl);
+						bl = null;
+					}
+					Z = 1;
+					continue;
+				}
+			}
+			if (Z == 1 || Z == 2 || Z == 3) {
+				Matcher m = matchLine(P2, line); // Begin of bookings section (Buchung, Valuta, ..., Soll, Haben)
+				if (m != null) {
+					if (bl != null) {
+						b.lines.add(bl);
+						bl = null;
+					}
+					Z = 2;
+					continue;
+				}
+			}
+			if (Z == 2 || Z == 3) {
+				Matcher m = matchLine(P3, line); // First booking line (Date Buchung, Date Valuta, ..., Amount)
+				if (m != null) {
+					if (bl != null) {
+						b.lines.add(bl);
+					}
+					bl = new BookingLine();
+					bl.setBuchung(m.group(1), b);
+					bl.setValuta(m.group(4), b);
+					bl.setText(m.group(7));
+					bl.setAmountTxt(m.group(8));
+					Z = 3;
+					continue;
+				}
+			}
+			if (Z == 3) {
+				Matcher m = matchLine(P4, line); // Last page, after last booking line (Filialnummer, ..., Neuer Saldo) 
+				if (m != null) {
+					if (bl != null) {
+						b.lines.add(bl);
+					}
+					bl = null;
+					break;
+				}
+			}
+			if (Z == 3 && !line.isEmpty()) { // Subsequent booking line text
+				bl.addText(line);
+				continue;
+			}
+		}
+		return b;
+	}
+
+	/**
+	 * Match the line against the supplied pattern.
+	 * 
+	 * @param p
+	 * @param line
+	 * @return If a match is present, return the Matcher so caller can access the
+	 *         groups, <code>null</code> on no match.
+	 */
+	protected Matcher matchLine(Pattern p, String line) {
+		Matcher matcher = p.matcher(line);
+		return matcher.matches() ? matcher : null;
+	}
+
+	/**
+	 * A booking has been assembled for a file. Emit it.
+	 * <p>
+	 * Depending on options, this will either be into individual JSON files, one per
+	 * booking line, in directory mode (-d).
+	 * <p>
+	 * Or, we just output the JSON to the output stream.
+	 * <p>
+	 * JSON is pretty-printed, unless one-line mode is used (-1).
+	 * 
+	 * @param b
+	 * @throws IOException
+	 */
+	protected void emitBooking(Booking b) throws IOException {
+		ObjectWriter writer = null;
+		if (oneLine) {
+			writer = new ObjectMapper().writer();
+		} else {
+			writer = new ObjectMapper().writerWithDefaultPrettyPrinter();
+		}
+		if (jsonMode) {
+			String fn = b.pdfFile.getName();
+			int lastDot = fn.lastIndexOf('.');
+			if (lastDot > 0) {
+				fn = fn.substring(0, lastDot);
+			}
+			fn = fn + ".json";
+			File bookingFile = new File(b.pdfFile.getParent(), fn);
+			if (!bookingFile.exists() || update) {
+				n += b.lines.size();
+				writer.writeValue(bookingFile, b);
+				if (verbose) {
+					out.println(bookingFile.toString());
+				}
+			}
+		} else if (baseDir != null) {
+			int nFile = 1;
+			String lastDate = "";
+			for (int i = 0; i < b.lines.size(); i++) {
+				BookingLine bl = b.lines.get(i);
+				String dt = buchungDate ? bl.buchung : bl.valuta;
+				if (!lastDate.equals(dt)) {
+					lastDate = dt;
+					nFile = 1;
+				}
+				String dd = dt.substring(0, 2);
+				String mm = dt.substring(3, 5);
+				String yyyy = dt.substring(6, 10);
+				File dir = new File(new File(baseDir, yyyy), mm);
+				if (!month) {
+					dir = new File(dir, dd);
+				}
+				String fn = String.format("%s-%s-%s-%08d.json", yyyy, mm, dd, nFile++);
+				File bookingLineFile = new File(dir, fn);
+				if (!bookingLineFile.exists() || update) {
+					dir.mkdirs();
+					// Include pointer to where this came from into the booking line JSON
+					bl.pdfName = b.pdfFile.getName();
+					bl.pdfLine = String.valueOf(i);
+					writer.writeValue(bookingLineFile, bl);
+					n++;
+					if (verbose) {
+						out.println(bookingLineFile.toString());
+					}
+				}
+			}
+		} else {
+			if (n > 0) {
+				// Empty line between files
+				out.print("\n");
+				n += b.lines.size();
+			}
+			writer.writeValue(out, b);
+		}
+	}
+
+	public static class PrefixFileFilter implements FileFilter {
+		private String prefix;
+
+		PrefixFileFilter(String prefix) {
+			this.prefix = prefix;
+		}
+
+		@Override
+		public boolean accept(File pathname) {
+			return pathname.isDirectory() || pathname.getName().startsWith(prefix);
+		}
+	}
+
+	void removeToUnicodeMaps(PDResources pdResources) throws IOException {
+		COSDictionary resources = pdResources.getCOSObject();
+
+		COSDictionary fonts = asDictionary(resources, COSName.FONT);
+		if (fonts != null) {
+			for (COSBase object : fonts.getValues()) {
+				while (object instanceof COSObject)
+					object = ((COSObject) object).getObject();
+				if (object instanceof COSDictionary) {
+					COSDictionary font = (COSDictionary) object;
+					font.removeItem(COSName.TO_UNICODE);
+				}
+			}
+		}
+
+		for (COSName name : pdResources.getXObjectNames()) {
+			PDXObject xobject = pdResources.getXObject(name);
+			if (xobject instanceof PDFormXObject) {
+				PDResources xobjectPdResources = ((PDFormXObject) xobject).getResources();
+				removeToUnicodeMaps(xobjectPdResources);
+			}
+		}
+	}
+
+	COSDictionary asDictionary(COSDictionary dictionary, COSName name) {
+		COSBase object = dictionary.getDictionaryObject(name);
+		return object instanceof COSDictionary ? (COSDictionary) object : null;
+	}
+
+	@JsonInclude(Include.NON_NULL) //
+	public static class BookingLine {
+		public String pdfName;
+		public String pdfLine;
+		private String buchung;
+		private String valuta;
+		private String text;
+		private String amountTxt;
+		private BigDecimal amount;
+
+		public String getBuchung() {
+			return buchung;
+		}
+
+		public void setBuchung(String buchung, Booking b) {
+			this.buchung = toFullDate(buchung, b);
+		}
+
+		public String getValuta() {
+			return valuta;
+		}
+
+		public void setValuta(String valuta, Booking b) {
+			this.valuta = toFullDate(valuta, b);
+		}
+
+		public String getAmount() {
+			return amountTxt;
+		}
+
+		public BigDecimal getAmountCt() {
+			return amount;
+		}
+
+		public void setAmountTxt(String amountTxt) {
+			this.amountTxt = amountTxt;
+			this.amount = new BigDecimal(amountTxt.replaceAll("[ \\.\\,]", ""));
+		}
+
+		public String getText() {
+			return text;
+		}
+
+		public void setText(String t) {
+			text = t;
+		}
+
+		public void addText(String t) {
+			text = text + " " + t;
+		}
+
+		protected String toFullDate(String dt4, Booking b) {
+			// This will not be accurate for bank statements that span calendar years.
+			// I do not have any like that, though.
+			return dt4 + b.from.substring(6);
+		}
+	}
+
+	public static class Booking {
+		public File pdfFile;
+		public String from;
+		public String to;
+		public List<BookingLine> lines = new ArrayList<>();
+	}
+}
+
+// The End
